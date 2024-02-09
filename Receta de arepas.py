025c742195acf0bdbc105d260ef20d7551d9c10f
marcosgg2005 <marcosgg2005@gmail.com>
2024-02-08 21:48:08 -0400

    Update Receta de arepas.py

--- Receta de arepas.py
+++ Receta de arepas.py
@@ -3,12 +3,12 @@ print (" para ello me vas a indicar la cantidad de ingredientes a implementar y 
 print ( "los ingredientes son: Agua, Harina PAN, cdt de sal" )
 
 harina=int (input("indica la cantidad de harina a utilizar en gramos:"))
-Agua= int (input("indica la cantidad de agua a utilizar en ml:"))
+agua= int (input("indica la cantidad de agua a utilizar en ml:"))
 sal= int (input("indica la cantidad de sal a utilizar en gramos:"))
 #1 ml de agua = 1g de agua
 # 1 cucharada = 12g 
  
-gramos= harina + Agua + sal 
+gramos= harina + agua+ sal 
 arepa= gramos / 65 
 
 print ("vas a obtener", arepa , "arepas")
